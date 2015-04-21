54b0d38718deb0f5ed1e2f95e8c4252767e223cf
Colin T.A. Gray <colinta@gmail.com>
2015-04-21 14:14:45 -0600

    version bump (Firebase.off removed)

--- lib/firebase/version.rb
+++ lib/firebase/version.rb
@@ -1,6 +1,6 @@
 module Motion
   module Firebase
-    Version = '3.1.3'  # motion-firebase gem version
+    Version = '3.1.4'  # motion-firebase gem version
     SdkVersion = '1.2.2'  # firebase framework version
   end
 end
