3378e80547d0c189fd0c4c8358552edd46779854
caramdache <caramdache@users.noreply.github.com>
2015-05-03 12:32:32 +0200

    Add missing Firebase URL

--- app-ios/app_delegate_nanostore.rb
+++ app-ios/app_delegate_nanostore.rb
@@ -1,3 +1,5 @@
+FirechatNS = 'https://firechat-ios.firebaseio-demo.com/'
+
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     return true if RUBYMOTION_ENV == 'test'
