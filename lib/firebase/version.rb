c932e7d019ecb2c7c3414c4736edf946571f439b
Colin T.A. Gray <colinta@gmail.com>
2015-05-15 14:17:25 -0600

    version bump

--- lib/firebase/version.rb
+++ lib/firebase/version.rb
@@ -1,6 +1,6 @@
 module Motion
   module Firebase
-    Version = '3.1.5'  # motion-firebase gem version
+    Version = '3.1.6'  # motion-firebase gem version
     SdkVersion = '2.2.1'  # firebase framework version
   end
 end
