0039626f997c8d5df0305726bf4df14a016c3e36
Colin T.A. Gray <colinta@gmail.com>
2015-10-08 15:29:19 -0600

    updates the Firebase framework to 2.4.1

--- lib/firebase/version.rb
+++ lib/firebase/version.rb
@@ -1,6 +1,6 @@
 module Motion
   module Firebase
     Version = '3.1.8'  # motion-firebase gem version
-    SdkVersion = '2.3.1'  # firebase framework version
+    SdkVersion = '2.4.1'  # firebase framework version
   end
 end
