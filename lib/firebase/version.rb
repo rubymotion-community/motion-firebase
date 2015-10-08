b561256ab701f474b49d7941065ac41af61e1c32
Colin T.A. Gray <colinta@gmail.com>
2015-10-08 15:23:31 -0600

    bump version, new facebook SDK code

--- lib/firebase/version.rb
+++ lib/firebase/version.rb
@@ -1,6 +1,6 @@
 module Motion
   module Firebase
-    Version = '3.1.7'  # motion-firebase gem version
+    Version = '3.1.8'  # motion-firebase gem version
     SdkVersion = '2.3.1'  # firebase framework version
   end
 end
