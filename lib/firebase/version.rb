7e75adb99c0011d7fcb834b2794da504e6caaaac
spnkr <wjessop@gmail.com>
2016-02-10 22:58:58 -0500

    updated Firebase SDK to 2.5.0

--- lib/firebase/version.rb
+++ lib/firebase/version.rb
@@ -1,6 +1,6 @@
 module Motion
   module Firebase
-    Version = '3.2.0'  # motion-firebase gem version
-    SdkVersion = '2.4.1'  # firebase framework version
+    Version = '3.2.1'  # motion-firebase gem version
+    SdkVersion = '2.5.0'  # firebase framework version
   end
 end
