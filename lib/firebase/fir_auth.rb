73cc6dd9b22278338fff61ae90e6fb47f0bec2dc
ljorgens <leejorgensen22@gmail.com>
2017-07-01 00:21:15 -0700

    add reauthentication, forgot password, change password

--- lib/firebase/fir_auth.rb
+++ lib/firebase/fir_auth.rb
@@ -17,9 +17,98 @@ class FIRAuth
     return self
   end
   
-  def login(email, password)
-    puts "Test"
+  def login(credentials, &and_then)
+    raise ":email is required in #{__method__}" unless credentials.key?(:email)
+    raise ":password is required in #{__method__}" unless credentials.key?(:password)
+    email = credentials[:email]
+    password = credentials[:password]
+    signInWithEmail(email, password: password, completion: and_then)
+    return self
+  end
+  
+  def self.open_facebook_session(options={}, &block)
+    self.new.open_facebook_session(options={}, &block)
+  end
+  
+  def open_facebook_session(options={}, &block)
+    ref = self
+    permissions = options[:permissions] || ['email']
+    fb_login = FBSDKLoginManager.alloc.init
+    fb_login.logInWithReadPermissions(permissions, 
+      handler: -> (facebookResult, facebookError) do
+        if facebookError
+          block.call(facebookError, nil)
+        elsif facebookResult.isCancelled
+          block.call("Facebook login got cancelled.", nil)
+        else
+          credentials = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken.tokenString)
+          ref.signInWithCredential(credentials, completion: block)
+        end
+      end)
+    nil
   end
 
+  def self.update_user_email(credentials, &block)
+    self.new.update_user_email(credentials, &block)
+  end
+  
+  def update_user_email(credentials, &block)
+    raise ":email is required in #{__method__}" unless credentials.key?(:email)
+    email = credentials[:email]
+    currentUser.updateEmail(email, completion: block)
+  end
+  
+  def self.update_password(credentials, &block)
+    self.new.update_user(credentials, &block)
+  end
+  
+  def update_password(credentials, &block)
+    raise ":new_password is required in #{__method__}" unless credentials.key?(:new_password)
+    new_password = credentials[:new_password]
+    currentUser.updatePassword(new_password, completion: block)
+  end
+  
+  def self.create_user_and_login(credentials, &block)
+    self.new.create_user_and_login(credentials, &block)
+  end
+  
+  def create_user_and_login(credentials, &block)
+    raise ":email is required in #{__method__}" unless credentials.key?(:email)
+    raise ":password is required in #{__method__}" unless credentials.key?(:password)
+    email = credentials[:email]
+    password = credentials[:password]
+    begin
+      createUserWithEmail(email, password: password, completion: -> (user, error) do
+        if error
+          block.call(error, nil)
+        else
+          login(credentials, &block)
+        end
+      end)
+    rescue RuntimeError => e
+      block.call(e, nil)
+    end
+  end
+  
+  def self.send_password_reset(credentials, &block)
+    self.new.send_password_reset(credentials, &block)
+  end
+  
+  def send_password_reset(email, &block)
+    sendPasswordResetWithEmail(email, completion: block)
+  end
+  
+  def check_provider_from_email(email, &block)
+    fetchProvidersForEmail(email, completion: block)
+  end
+  
+  def reauthenticate_user(credentials, &block)
+    raise ":email is required in #{__method__}" unless credentials.key?(:email)
+    raise ":password is required in #{__method__}" unless credentials.key?(:password)
+    email = credentials[:email]
+    password = credentials[:password]
+    newCredentials = FIREmailPasswordAuthProvider.credentialWithEmail(email, password: password)
+    currentUser.reauthenticateWithCredential(newCredentials, completion: block)
+  end
 
 end
--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -1,11 +1,11 @@
 describe 'MotionFirebase utilities' do
   describe 'convert_event_type' do
     [
-      [[:child_added, :added], FEventTypeChildAdded],
-      [[:child_moved, :moved], FEventTypeChildMoved],
-      [[:child_changed, :changed], FEventTypeChildChanged],
-      [[:child_removed, :removed], FEventTypeChildRemoved],
-      [[:value], FEventTypeValue],
+      [[:child_added, :added], FIRDataEventTypeChildAdded],
+      [[:child_moved, :moved], FIRDataEventTypeChildMoved],
+      [[:child_changed, :changed], FIRDataEventTypeChildChanged],
+      [[:child_removed, :removed], FIRDataEventTypeChildRemoved],
+      [[:value], FIRDataEventTypeValue],
     ].each do |symbols, actual|
       symbols.each do |sym|
         it("should convert #{sym.inspect}") do
